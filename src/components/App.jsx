399b50f6e8b7efd5c737f09fb8285c7fdb637554
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-07-24 21:17:46 -0300

    Adicionados tags de título e de meta, remoção do favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import List from './List';
 import ListAddItemForm from './ListAddItemForm';
 import ListProvider from './ListProvider';
+import Helmet from 'react-helmet';
 import '../styles/App.css';
 
 function App() {
   return (
     <div class="content">
+      <Helmet>
+        <title>Lista ToDo</title>
+
+        <meta name="author" content="Arthur Dev" />
+        <meta name="description" content="Aplicativo que faz uma lista de tarefas" />
+        <meta name="keywords" content="ToDo, lista, tarefas, tasks, listagem" />
+      </Helmet>
+
       <ListProvider>
         <ListAddItemForm />
 
